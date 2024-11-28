776f13877e0eb135733462a73a96e0fedb866ecb
dreamY <3350057050@QQ.com>
2024-11-28 20:18:53 +0800

    1.添加player，并添加动画。2.使用状态机添加了Idle和Walk两个状态之间的逻辑和动画切换

--- Script/State_Machine/node_state_machine.gd
+++ Script/State_Machine/node_state_machine.gd
@@ -3,20 +3,22 @@ extends Node
 
 @export var initial_node_state : NodeState
 
-var node_states : Dictionary = {}
-var current_node_state : NodeState
-var current_node_state_name : String
-var parent_node_name: String
+var node_states : Dictionary = {} # 一个字典用于存储状态机的全部节点
+var current_node_state : NodeState #当前状态节点
+var current_node_state_name : String #当前状态节点名称 
+var parent_node_name: String #父节点名称 用于测试程序 说明这是哪个角色的状态机
 
 
 func _ready() -> void:
 	parent_node_name = get_parent().name
 	
+	#获取状态机节点下的所有状态，并将其存入node_states这个字典中，连接transition这个信号到transition_to这个函数
 	for child in get_children():
 		if child is NodeState:
 			node_states[child.name.to_lower()] = child
 			child.transition.connect(transition_to)
 	
+	#设置初始状态
 	if initial_node_state:
 		initial_node_state._on_enter()
 		current_node_state = initial_node_state
@@ -25,6 +27,7 @@ func _ready() -> void:
 
 func _process(delta : float) -> void:
 	if current_node_state:
+		#print("Current State: ", current_node_state_name)
 		current_node_state._on_process(delta)
 
 
@@ -34,11 +37,13 @@ func _physics_process(delta: float) -> void:
 		current_node_state._on_next_transitions()
 		#print(parent_node_name, " Current State: ", current_node_state_name)
 
-
+#通过emit("***")其中的字符 转化到对应的节点
 func transition_to(node_state_name : String) -> void:
+	# 如果状态没有发生变化直接返回
 	if node_state_name == current_node_state.name.to_lower():
 		return
 	
+	#字典里的名字获取到对应的节点
 	var new_node_state = node_states.get(node_state_name.to_lower())
 	
 	if !new_node_state:
