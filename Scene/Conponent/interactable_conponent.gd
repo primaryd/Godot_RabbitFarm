2aec3a78f478f1f227b7ea693618a2b2b7cb1140
dreamY <3350057050@QQ.com>
2024-11-28 21:49:22 +0800

    1.添加房屋，并设置了门  2.InteracableConponent可交互组件创建（未完成）

---
+++ Scene/Conponent/interactable_conponent.gd
@@ -0,0 +1,16 @@
+class_name InteractableConponent
+extends Area2D
+
+signal interactable_actived
+signal interactable_deactived
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+	
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
